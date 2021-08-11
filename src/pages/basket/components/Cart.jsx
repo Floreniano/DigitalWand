27f856cd33ac5719d7f29fe0add4ac3f3079e9eb
Floreniano <florikinka@gmail.com>
2021-08-11 17:53:50 +0300

    editBasketPage

--- src/pages/basket/components/Cart.jsx
+++ src/pages/basket/components/Cart.jsx
@@ -35,44 +35,6 @@ function Cart({
           trigger={<span className="card__product-remove">Remove</span>}
           text={'Вы действительно хотите удалить выбранный товар?'}
         />
-        <div className="card__product-operations">
-          <button className="card__product-operations-btn minus" disabled={totalCount === 1} onClick={handleMinusItem}>
-            <svg
-              width="10"
-              height="10"
-              viewBox="0 0 10 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                fill="#6A983C"
-              />
-              <path
-                d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                fill="#6A983C"
-              />
-            </svg>
-          </button>
-          <button className="card__product-operations-btn plus" onClick={handlePlusItem}>
-            <svg
-              width="10"
-              height="10"
-              viewBox="0 0 10 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                fill="#6A983C"
-              />
-              <path
-                d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                fill="#6A983C"
-              />
-            </svg>
-          </button>
-        </div>
       </div>
       <div className="card__product-info">
         <h3 className="card__product-title">{name}</h3>
@@ -92,9 +54,59 @@ function Cart({
           <StarList ratingStars={rating} />
         </div>
         <div className="card__product-cost">
-          <div className="card__product-cost-text">
-            <span className="card__product-cost-text-price">{totalPrice} ₽</span>
-            <span className="card__product-cost-text-count"> ({totalCount} шт)</span>
+          <div className="card__product-cost-total">
+            <span className="card__product-cost-total-price">{totalPrice} ₽</span>
+            <div className="card__product-cost-total-count">
+              <button
+                className="btn-operation minus"
+                disabled={totalCount === 1}
+                onClick={handleMinusItem}
+              >
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 10 10"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                    fill="#6A983C"
+                  />
+                  <path
+                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                    fill="#6A983C"
+                  />
+                </svg>
+              </button>
+              {/* <input
+                type="text"
+                className="card__product-cost-total-count-text"
+                maxLength="2"
+                defaultValue={totalCount}
+                disabled
+              ></input> */}
+              <span className="card__product-cost-total-count-text">{totalCount}</span>
+              <button className="btn-operation plus" onClick={handlePlusItem}>
+                <svg
+                  width="10"
+                  height="10"
+                  viewBox="0 0 10 10"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                    fill="#6A983C"
+                  />
+                  <path
+                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                    fill="#6A983C"
+                  />
+                </svg>
+              </button>
+            </div>
+
             {/* Скидка <span className="card__product-cost-text-discount">48.56 ₽</span> */}
           </div>
           <div className="card__product-cost-count">
